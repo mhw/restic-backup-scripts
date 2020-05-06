fa005ee57767fde5ee0ac636e7192ee0b033d737
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2020-05-06 22:44:02 +0000

    PostgreSQL backup script

---
+++ postgresql-backup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+. ./common.sh
+
+TAG=postgresql
+
+for DB in $(psql -l | \
+	awk '{print $1}' | \
+	grep -Ev "^(List|Name|-*\+|postgres|template|\||\(|$)")
+do
+	pg_dump --create --clean --if-exists --no-owner --no-privileges $DB | \
+		gzip --rsyncable | \
+		restic backup \
+			--stdin --stdin-filename postgresql/$DB.sql.gz \
+		       	--tag "$TAG" \
+		       	--tag "$DB"
+done
