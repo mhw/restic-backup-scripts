5d961e480d2ae2836aa4c26af3031dfa01fb23cb
Mark H. Wilkinson <mhw@dangerous-techniques.com>
2020-05-06 14:51:46 +0000

    gzip the sql
    But in a way that should allow restic to find duplicate blocks if the
    file has similarities.

--- mysql-backup.sh
+++ mysql-backup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash -x
+#
 
 TAG=mysql
 
 for DB in $(mysql -u$USER -BNe 'show databases' | grep -Ev 'mysql|information_schema|performance_schema')
 do
 	mysqldump -u$USER --force $DB | \
+		gzip --rsyncable | \
 		restic backup \
-			--stdin --stdin-filename mysql/$DB.sql \
+			--stdin --stdin-filename mysql/$DB.sql.gz \
 		       	--tag "$TAG" \
 		       	--tag "$DB" \
 			--quiet
